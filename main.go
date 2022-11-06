82f3abae64e65364a1c4e11d0ea60e898d18477d
Konstantin8105 <konstantin8105@gmail.com>
2022-11-06 17:09:28 +0300

    add clone flag

--- main.go
+++ main.go
@@ -4,17 +4,24 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"strings"
 )
 
 func main() {
-	// get json from github. Example:
-	// https://api.github.com/users/Konstantin8105/repos?page=$PAGE&per_page=1000
-	//                              --------------
-	//                                 User name
-	var userName = flag.String("u", "", "set the user name of the github repository")
+	var (
+		// get json from github. Example:
+		// https://api.github.com/users/Konstantin8105/repos?page=$PAGE&per_page=1000
+		//                              --------------
+		//                                 User name
+		userName = flag.String("u", "", "set the user name of the github repository")
+
+		// clone
+		clone = flag.Bool("clone", false, "clone all repositories")
+	)
 
 	flag.Parse()
 
@@ -25,6 +32,12 @@ func main() {
 	}
 	for _, repos := range rs {
 		fmt.Fprintf(os.Stdout, "%s\n", repos)
+		if *clone {
+			_, err := exec.Command("git", "clone", repos).Output()
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
 	}
 }
 
